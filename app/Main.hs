893e121c7e3fd35e915168e1ae85cd4eb71e3359
Thomas Dorval <72532514+thomas-dorval@users.noreply.github.com>
2026-05-07 18:27:30 -0400

    formatting

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,7 @@ import InputHelper (inputHelp)
 
 main :: IO ()
 main = do
-  putStrLn "==============================\n\tHaskell Connect-4 Project\n\tBy Thomas Dorval\n==============================\n"
+  putStrLn "============================================================\n\tHaskell Connect-4 Project\n\tBy Thomas Dorval\n============================================================\n"
   currentGameState
   inputHelp "Are you sure you want to exit? (y/n)"
             ['y', 'n']
